2bd33bdafc8c71d7f59f48a021f5a46471052fef
tylercmac <tylercmac@gmail.com>
2021-05-21 17:37:05 -0700

    created parallax and title

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+export default function About() {
+  return (
+    <div className='about'>
+      <h3 className='about-header'>About Me</h3>
+      <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.
+        
+      </p>
+    </div>
+  )
+}
---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+export default function Title() {
+  return (
+  
+        <div className="inner">
+          <div className='title-card'>
+          <h1 className="whitney">Ty McFarland</h1>
+          <hr class="solid" />
+          <h3 className="gotham">Exploring the world through design and logic</h3>
+          </div>
+        </div>
+
+  )
+}
