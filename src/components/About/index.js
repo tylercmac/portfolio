43928ea723830bf71ff4a6083e42e55d2a80e6ea
tylercmac <tylercmac@gmail.com>
2022-06-10 18:49:05 -0400

    Update about me and tech highlights

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,10 @@ import "./style.css";
 import avatar from './img/headshot.jpg';
 import mongodb from './img/pngjoy.com_mongodb-mongodb-icon-png-download_7091544.png';
 import express from './img/expressjs.png';
-import fastify from './img/fastify.png';
+import csharp from './img/c-sharp.svg';
+import remix from './img/remix.png';
+import materialui from './img/materialui.png';
+import aws from './img/aws.svg';
 
 export default function About() {
   return (
@@ -15,24 +18,27 @@ export default function About() {
       </div>
       <div className="row">
         <div className='col-lg-6 col-md-12'>
-          <p>My name is Ty, and I am a web developer out of the University of Washington's Full-Stack Web Engineering Certification program. When it comes to web applications, I have a high attention to detail and design on the front end, and a determination for efficency and flexibility on the back end. </p> <p>When I am not developing apps, I am usually either skiing, running, planning a mountaineering trip, teaching guitar and piano, or going on walks. Programming is my passion, but I could not enjoy it nearly as much if I was not able to get out into the backcountry every so often. I currently live just north of Spokane, WA in a cozy corner of the woods with my wife and Malamute. </p>
+          <p>My name is Ty, and I am a software engineer with a full-stack certificate from the University of Washington. For whatever tech role I inhabit, I bring a broad, yet detailed experience, such as maintaining and implementing a new RDS, working DevOps in AWS, spear-heading UI/UX features amidst juggling app support issues, all the way to a deep involvement in an entire replatforming of an existing enterprise application</p> 
+          
+          <p>When I am not exercising my keyboard calisthenics, I am usually either skiing, running, mountaineering, teaching/playing guitar and piano, or going on walks. Programming is my passion, but I could not enjoy it nearly as much if I was not able to get out into the backcountry every so often. I currently live in Richmond, VA, tucked away in a cozy suburb with my wife and 100 pound Alaskan Malamute. </p>
+
         </div>
         <div className='col-lg-6 col-md-12'>
-          <h2 className='text-center'>SKILLSET</h2>
+          <h2 className='text-center'>TECH HIGHLIGHTS</h2>
           <div className='skills'>
             <div className='col-lg-12 d-flex justify-content-around flex-wrap'>
-              <div className="skill-icon"><i className="fas fa-code fa-3x"></i><p className='skill-text'>HTML</p></div>
-              <div className="skill-icon"><i className="fab fa-css3 fa-3x"></i><p className='skill-text'>CSS</p></div>
-              <div className="skill-icon"><i className="fab fa-bootstrap fa-3x"></i><p className='skill-text'>Bootstrap</p></div>
               <div className="skill-icon"><i className="fab fa-js fa-3x"></i><p className='skill-text'>JavaScript</p></div>
-              <div className="skill-icon"><i className="fab fa-react fa-3x"></i><p className='skill-text'>React.js</p></div>
+              <div className="skill-icon"><img src={csharp} alt="csharp icon" className="mongoimg" /><p className='skill-text'>C#</p></div>
+              <div className="skill-icon"><img src={materialui} alt="materialui icon" className="mongoimg"/><p className="skill-text">MaterialUI</p></div>
+              <div className="skill-icon"><i className="fab fa-react fa-3x"></i><p className='skill-text'>React</p></div>
+              <div className="skill-icon"><img src={remix} alt="remix icon" className="mongoimg"/><p className="skill-text">Remix</p></div>
             </div>
             <div className='col-lg-12 d-flex justify-content-around flex-wrap'>
               <div className="skill-icon"><i className="fab fa-node-js fa-3x"></i><p className='skill-text'>Node.js</p></div>
               <div className="skill-icon"><img src={express} alt="express icon" className="expressimg" /><p className='skill-text'>Express.js</p></div>
-              <div className="skill-icon"><img src={fastify}  alt="fastify icon" className="mongoimg" /><p className='skill-text'>Fastify</p></div>
-              <div className="skill-icon"><i className="fas fa-database fa-3x"></i><p className='skill-text'>MySQL</p></div>
-              <div className="skill-icon"><img src={mongodb}  alt="mongod icon" className="mongoimg" /><p className='skill-text'>MongoDB</p></div>
+              <div className="skill-icon"><img src={aws}  alt="mongod icon" className="expressimg" /><p className='skill-text'>AWS</p></div>              
+              <div className="skill-icon"><i className="fas fa-database fa-3x"></i><p className='skill-text'>SQL</p></div>
+              <div className="skill-icon"><img src={mongodb}  alt="mongod icon" className="expressimg" /><p className='skill-text'>MongoDB</p></div>
             </div>
           </div>
         </div>
