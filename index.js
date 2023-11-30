cbb9b91b7bf9f61186650164a68f94c36f333095
tylercmac <tylercmac@gmail.com>
2023-11-29 20:29:16 -0500

    Move entry file to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '.src/index.css';
+import App from './src/App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root')
